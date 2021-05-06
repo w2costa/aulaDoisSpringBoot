a604e6892c96abee35a13057a2b86d2233ebecb0
Wilson Wolf Costa <wilson.w.costa@gmail.com>
2021-05-05 21:53:50 -0300

    CRUD pronto

--- src/main/java/com/example/aulaDoisSpringBoot/controller/ClienteController.java
+++ src/main/java/com/example/aulaDoisSpringBoot/controller/ClienteController.java
@@ -8,11 +8,17 @@ package com.example.aulaDoisSpringBoot.controller;
 import com.example.aulaDoisSpringBoot.model.Cliente;
 import com.example.aulaDoisSpringBoot.repository.ClienteRepository;
 import java.util.List;
-import org.springframework.http.HttpStatus;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -21,17 +27,53 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
-    
+
     private final ClienteRepository clienteRepository;
+    private final String CLIENTE_URI = "clientes/";
 
     public ClienteController(ClienteRepository clienteRepository) {
         this.clienteRepository = clienteRepository;
     }
-    
+
     @RequestMapping("")
     public ModelAndView page(Model model) {
         List<Cliente> clientes = clienteRepository.findAll();
-        return new ModelAndView("clientes/list", "clientes", clientes);
+        return new ModelAndView(CLIENTE_URI + "list", "clientes", clientes);
     }
-    
+
+    @RequestMapping("{id}")
+    public ModelAndView view(@PathVariable("id") Cliente cliente) {
+        return new ModelAndView(CLIENTE_URI + "view", "cliente", cliente);
+    }
+
+    @RequestMapping("/novo")
+    public String novo(@ModelAttribute Cliente cliente) {
+        return CLIENTE_URI + "form";
+    }
+
+    @PostMapping(params = "form")
+    public ModelAndView salvar(@Valid Cliente cliente, BindingResult result, RedirectAttributes redirect) {
+        if (result.hasErrors()) {
+            return new ModelAndView(CLIENTE_URI + "form", "formErrors", result.getAllErrors());
+        }
+        cliente = clienteRepository.saveAndFlush(cliente);
+        redirect.addFlashAttribute("globalMessage", "Cliente gravado com sucesso.");
+        return new ModelAndView("redirect:/" + CLIENTE_URI + "{cliente.id}", "cliente.id", cliente.getId());
+    }
+
+    @GetMapping("remover/{id}")
+    public ModelAndView remover(@PathVariable("id") int id, RedirectAttributes redirect) {
+        clienteRepository.deleteById(id);
+        Iterable<Cliente> clientes = clienteRepository.findAll();
+
+        ModelAndView mv = new ModelAndView(CLIENTE_URI + "list", "clientes", clientes);
+        mv.addObject("globalMessage", "Cliente removido com sucesso.");
+
+        return mv;
+    }
+
+    @GetMapping("alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") Cliente cliente) {
+        return new ModelAndView(CLIENTE_URI + "form", "cliente", cliente);
+    }
 }
